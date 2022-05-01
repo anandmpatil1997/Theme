9f8033239c1d836b871ea514082ba4603a892c4f
anandmpatil1997 <96177274+anandmpatil1997@users.noreply.github.com>
2022-05-01 11:03:08 +0530

    first commit

---
+++ src/components/body.jsx
@@ -0,0 +1,20 @@
+export const Body=()=>{
+    return (
+
+        <div style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+            
+        }}>
+        <img style={{
+            width:"350px",
+            height:"350px",
+            borderRadius:"400px"
+        }} src="https://c.tenor.com/FyvK4Dx21McAAAAC/mochi-food.gif" alt="" />
+  
+        </div>
+    )
+}
